3753afd58a93bf028bbb781c76f1c6e44acbc2ac
Nicolas Gryman <ngryman@gmail.com>
2023-02-27 09:01:31 +0100

    refact: remove unused useTheme

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,21 +1,18 @@
-import { createEditor } from '@mindraft/create-editor'
+import { createEditor, Editor as EditorClass } from '@mindraft/create-editor'
 import { Editor } from '@mindraft/editor'
 import { markdownExtension } from '@mindraft/editor-extension-markdown'
 import {
   persistenceExtension,
   usePersistence,
 } from '@mindraft/editor-extension-persistence'
-import { EditorTheme } from '@mindraft/editor-theme'
 import { createAtom } from '@mindraft/utils'
 import { Presence } from '@motionone/solid'
 import { createShortcut } from '@solid-primitives/keyboard'
 import { createResource, Show } from 'solid-js'
-import { useTheme } from 'solid-styled-components'
 
 import { Pane, Workspace } from './layout'
 
 export function App() {
-  const theme = useTheme() as EditorTheme
   const { loadDocument, saveDocument } = usePersistence()
   const editor = createEditor([
     markdownExtension(),
