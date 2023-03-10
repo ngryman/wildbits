f9448572187802f32c9f9431ac8ca01c4cafcbc1
Nicolas Gryman <ngryman@gmail.com>
2023-03-10 23:38:36 +0100

    chore: better css modules integration

--- packages/editor/src/env.d.ts
+++ packages/editor/src/env.d.ts
@@ -1,1 +1,5 @@
-declare module '*.module.css'
+declare module '*.module.css' {
+  type CSSModuleClasses = { readonly [key: string]: string }
+  const classes: CSSModuleClasses
+  export default classes
+}
