d6f753f8ec19b56f0e5a282495dd2fb1143402bc
Nicolas Gryman <ngryman@gmail.com>
2023-03-11 18:32:26 +0100

    ui(editor): fill the whole height

--- app/src/layout/pane.tsx
+++ app/src/layout/pane.tsx
@@ -1,9 +1,12 @@
 import { Motion } from '@motionone/solid'
 import { ParentProps } from 'solid-js'
 
+import styles from './pane.module.css'
+
 export function Pane(props: ParentProps) {
   return (
     <Motion.div
+      class={styles.root}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.3 }}
