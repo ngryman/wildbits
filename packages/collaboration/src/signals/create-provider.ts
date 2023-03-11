ef6ff0102e87ed8121468e3ccbf3e44238423ae9
Nicolas Gryman <ngryman@gmail.com>
2023-03-11 17:20:28 +0100

    try: explicitly set STUN servers

--- packages/collaboration/src/signals/create-provider.ts
+++ packages/collaboration/src/signals/create-provider.ts
@@ -7,6 +7,14 @@ export function createProvider(settings: Settings): Provider {
   const document = new Doc()
   const webrtcProvider = new WebrtcProvider(settings.id, document, {
     signaling: [settings.signalingServer],
+    peerOpts: {
+      config: {
+        iceServers: [
+          { urls: 'stun:stun.l.google.com:19302' },
+          { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
+        ],
+      },
+    },
   })
 
   return {
