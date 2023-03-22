06bb9447f78a27509de11d236c52f5b26faa1ac8
Nicolas Gryman <ngryman@gmail.com>
2023-03-22 12:51:57 +0100

    test: reduce default cy timeout to 2s

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   video: false,
   e2e: {
     baseUrl: 'http://127.0.0.1:3000',
+    defaultCommandTimeout: 2000,
     screenshotsFolder: 'tests/cypress/screenshots',
     specPattern: '{features,packages}/*/tests/**/*.spec.ts',
     supportFile: 'tests/cypress/support/e2e.ts',
