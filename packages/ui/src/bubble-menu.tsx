640c4bb35b52929bb81f0676f649a877647aaf1f
Nicolas Gryman <ngryman@gmail.com>
2023-03-28 15:25:27 +0200

    ui: make sure bubble menu is fully visible

--- packages/ui/src/bubble-menu.tsx
+++ packages/ui/src/bubble-menu.tsx
@@ -1,4 +1,5 @@
 import { createMemo, onMount, ParentProps } from 'solid-js'
+import { remToPx } from '@wildbits/utils'
 
 import styles from './bubble-menu.module.css'
 
@@ -6,18 +7,31 @@ type Props = ParentProps & {
   bounds?: DOMRect
 }
 
+const PADDING = remToPx(1)
+
 export function BubbleMenu(props: Props) {
   let ref!: HTMLDivElement
   let refBounds!: DOMRect
 
   const style = createMemo(() => {
-    if (!props.bounds || !refBounds) return undefined
+    const { bounds } = props
 
-    const left = props.bounds.left + (props.bounds.width - refBounds.width) / 2
+    // By default the menu will be displayed above the first ancestor with a
+    // `position: relative`.
+    if (!bounds || !refBounds) return undefined
+
+    let top = bounds.top - refBounds.height - PADDING
+    if (top < 0) {
+      top = bounds.top + bounds.height + PADDING
+    }
+
+    const left = bounds.left + (bounds.width - refBounds.width) / 2
 
     return {
-      top: `calc(${props.bounds.top}px - 1rem)`,
+      top: `${top}px`,
       left: `${left}px`,
+      // Override default
+      translate: '0 0',
     }
   })
 
