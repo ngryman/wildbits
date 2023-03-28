d3a7c6979c9895e248eeb4175c773249e20eb9bb
Nicolas Gryman <ngryman@gmail.com>
2023-03-28 15:27:37 +0200

    refact(float-menu): make the show logic easier to reason about

--- features/floating-menu/src/components/floating-menu.tsx
+++ features/floating-menu/src/components/floating-menu.tsx
@@ -1,7 +1,7 @@
 import { findParentNodeClosestToPos, isTextSelection } from '@tiptap/core'
 import { BubbleMenu } from '@wildbits/ui'
 import { PluginViewProps } from '@wildbits/utils'
-import { createMemo, For, Show } from 'solid-js'
+import { createMemo, For, on, Show } from 'solid-js'
 
 import { FloatingMenuOptions } from '../extension'
 import { ActionItem } from './action-item'
@@ -9,25 +9,33 @@ import { ActionItem } from './action-item'
 const emptyRect = new DOMRect(0, 0, 0, 0)
 
 export function FloatingMenu(props: PluginViewProps<FloatingMenuOptions>) {
-  const show = () => {
-    const { doc, selection } = props.state
-    const { $anchor, empty, from, to } = selection
+  const show = createMemo(
+    on(
+      () => props.state.selection,
+      () => {
+        const { doc, selection } = props.state
+        const { $anchor, empty, from, to } = selection
 
-    // NOTE: Sometime check for `empty` is not enough. For example, double
-    // clicking an empty paragraph returns a node size of 2. So we also check
-    // for an empty text size.
-    const isEmptyTextBlock =
-      !doc.textBetween(from, to).length && isTextSelection(props.state.selection)
+        // Bail out if the selection is empty...
+        if (empty) return false
+        // ...or it's not a text selection...
+        if (!isTextSelection(selection)) return false
+        // ...or the selected text is not empty...
+        // NOTE: Sometime check for `empty` is not enough. For example, double
+        // clicking an empty paragraph returns a node size of 2. So we also check
+        // for an empty text size.
+        if (doc.textBetween(from, to).length === 0) return false
+        // ...or we're in a code block
+        // TODO: We might want to provide a way to configure that instead of hard
+        // coding it here.
+        if (findParentNodeClosestToPos($anchor, node => node.type.name === 'codeBlock'))
+          return false
 
-    // Hide in code blocks.
-    // TODO: We might want to provide a way to configure that instead of hard
-    // coding it here.
-    const isInCodeBlock = Boolean(
-      findParentNodeClosestToPos($anchor, node => node.type.name === 'codeBlock')
-    )
-
-    return props.editor.view.hasFocus() && !empty && !isEmptyTextBlock && !isInCodeBlock
-  }
+        return true
+      }
+    ),
+    false
+  )
 
   const bounds = createMemo<DOMRect>(prevBounds => {
     if (!show()) return emptyRect
